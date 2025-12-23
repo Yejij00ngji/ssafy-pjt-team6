954b5dc224274194133b1bc0b6e0ae48d0f8c2e7
Aorok <aethersnest@gmail.com>
2025-12-23 15:46:10 +0900

    Feat: 상품 기능 화면 구현

--- Backend/products/filters.py
+++ Backend/products/filters.py
@@ -2,10 +2,15 @@ from django_filters import rest_framework as filters
 from .models import DepositProducts
 
 class ProductFilter(filters.FilterSet):
-  bank = filters.CharFilter(field_name="kor_co_nm", lookup_expr='exact')
+  bank = filters.CharFilter(field_name="kor_co_nm", lookup_expr='icontains')
+
+  product_type = filters.ChoiceFilter(
+        field_name="product_type",
+        choices=(('DEPOSIT', '정기예금'), ('SAVING', '적금'))
+    )
 
   term = filters.NumberFilter(field_name="options__save_trm", lookup_expr='contains')
 
   class Meta:
     model = DepositProducts
-    fields = ['bank', 'term']
+    fields = ['bank', 'product_type', 'term']
